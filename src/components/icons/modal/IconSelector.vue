5642c2b4009074c7319aecc27546fa473f6ff6b5
João Pedro Ennes <65932730+EnnesJp@users.noreply.github.com>
2024-01-16 00:23:44 -0300

    improve: improving icon components

---
+++ src/components/icons/modal/IconSelector.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import IconCard from '@/components/icons/modal/IconCard.vue'
+import IconCoin from '@/components/icons/modal/IconCoin.vue'
+import IconDelete from '@/components/icons/modal/IconDelete.vue'
+import IconSave from '@/components/icons/modal/IconSave.vue'
+import IconSuccess from '@/components/icons/modal/IconSuccess.vue'
+
+const props = defineProps({
+  icon: String
+})
+</script>
+
+<template>
+  <IconCard v-if="icon == 'card'"/>
+  <IconCoin v-else-if="icon == 'coin'"/>
+  <IconDelete v-else-if="icon == 'delete'"/>
+  <IconSave v-else-if="icon == 'save'"/>
+  <IconSuccess v-else/>
+</template>
